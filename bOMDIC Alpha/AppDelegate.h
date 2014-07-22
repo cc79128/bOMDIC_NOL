4eba4f545296cf1569ff1f6eacf1028fe77259d7
Scott <Scott@ScotttekiMacBook-Pro.local>
2014-07-22 16:32:42 +0800

    Initial Commit

---
+++ bOMDIC Alpha/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bOMDIC Alpha
+//
+//  Created by Scott on 2014/7/22.
+//  Copyright (c) 2014年 bOMDIC Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bOMDIC AlphaTests/bOMDIC_AlphaTests.m
@@ -0,0 +1,34 @@
+//
+//  bOMDIC_AlphaTests.m
+//  bOMDIC AlphaTests
+//
+//  Created by Scott on 2014/7/22.
+//  Copyright (c) 2014年 bOMDIC Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bOMDIC_AlphaTests : XCTestCase
+
+@end
+
+@implementation bOMDIC_AlphaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
